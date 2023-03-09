90708502884688a350e75aec33c3a43ef384a9bd
Josiah Elliott <josiahelliott@Josiahs-MacBook-Pro.local>
2023-03-08 21:21:39 -0600

    add react router, initial errorBoundary

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import React from "react";
+import * as React from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import App from "./App";
+import ErrorBoundary from "./components/errorBoundary";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<App />} path={"/"} errorElement={<ErrorBoundary />} />
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
